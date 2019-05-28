cb2d71c8ed8bdf0cf7c29e76e2b96b7107a8ba65
Mokey <MokeyII@users.noreply.github.com>
2019-05-28 15:56:32 -0400

    more

--- eve_sql.sql
+++ eve_sql.sql
@@ -6,3 +6,6 @@ SELECT character_id, skill_id, trained_skill_level FROM character_skills WHERE c
 
 /*SELECT PlayerID and skill levels to match*/
 SELECT character_id, skill_id, active_skill_level FROM character_skills WHERE character_id = 92692957 AND active_skill_level =< 4;
+
+/*Find PlayerID, SkillID, and check level*/
+SELECT character_skills.character_id, character_skills.trained_skill_level, character_skills.skill_id FROM character_skills INNER JOIN character_infos ON character_infos.character_id = character_skills.character_id WHERE character_infos.character_id = 92692957 AND character_skills.active_skill_level <5 AND skill_id = 3361;
