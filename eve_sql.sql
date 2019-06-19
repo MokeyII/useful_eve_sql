1bc6a0d6f7706b85b7a7ecb3bb537ba7209bbc33
Mokey <MokeyII@users.noreply.github.com>
2019-06-19 12:13:07 -0400

    Update eve_sql.sql

--- eve_sql.sql
+++ eve_sql.sql
@@ -9,4 +9,25 @@ SELECT character_id, skill_id, active_skill_level FROM character_skills WHERE ch
 
 /*Find PlayerID, SkillID, and check level*/
 SELECT character_skills.character_id, character_skills.trained_skill_level, character_skills.skill_id FROM character_skills INNER JOIN character_infos ON character_infos.character_id = character_skills.character_id WHERE character_infos.character_id = 92692957 AND character_skills.active_skill_level <5 AND skill_id = 3361;
+
+
 SELECT name, invTypes.typeName, character_skills.trained_skill_level FROM character_skills INNER JOIN character_infos ON character_infos.character_id = character_skills.character_id INNER JOIN invTypes ON character_skills.skill_id = invTypes.typeID WHERE character_skills.active_skill_level <5 AND skill_id = 12203 AND name = 'Mokey PYD';
+
+
+/*Select All players skills, list by Name, typeName, and Trained Skill Level */
+SELECT name, invTypes.typeName, character_skills.trained_skill_level 
+	FROM character_skills 
+INNER JOIN character_infos 	
+	ON character_infos.character_id = character_skills.character_id 
+INNER JOIN invTypes 
+	ON character_skills.skill_id = invTypes.typeID;
+  
+ /*Select player passing parameters*/
+ SELECT name, invTypes.typeName, character_skills.trained_skill_level 
+  FROM character_skills 
+ INNER JOIN character_infos
+  ON character_infos.character_id = character_skills.character_id 
+ INNER JOIN invTypes 
+  ON character_skills.skill_id = invTypes.typeID 
+  WHERE character_skills.active_skill_level <5 AND skill_id = 12203 AND name = 'Mokey PYD';
+
